09361a4bcb2670cb0a3789f260c2e89cf607f229
Milan Samardzic <milan.samardzic@klika.ba>
2015-04-27 23:13:03 +0200

    some fixing

--- app/src/main/java/com/milansamardzic/food2fork/Have.java
+++ app/src/main/java/com/milansamardzic/food2fork/Have.java
@@ -2,14 +2,18 @@ package com.milansamardzic.food2fork;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Outline;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.andexert.library.RippleView;
@@ -27,41 +31,31 @@ import java.util.ArrayList;
  * Created by ms on 1/22/15.
  */
 public class Have extends android.support.v4.app.Fragment {
-    private ListView lvMovies;
+    private ListView lvRecepti;
     private ReceptiAdapter adapterMovies;
     private Fork2FoodClient client;
     public static final String MOVIE_DETAIL_KEY = "recipes";
     EditText et1, et2, et3;
     public int helpper=0;
     // ImageButton fabS;
-
-    FloatingActionButton fabBtn;
-    FloatingActionButton fabSbtn;
-
+    FloatingActionButton fabS;
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.have, container, false);
-        lvMovies = (ListView) rootView.findViewById(R.id.lvRecepti);
+        lvRecepti = (ListView) rootView.findViewById(R.id.lvRecepti);
 
-<<<<<<< HEAD
-        fabBtn = (FloatingActionButton) rootView.findViewById(R.id.fabBtn);
-        fabSbtn = (FloatingActionButton) rootView.findViewById(R.id.fabbuttonSearch);
 
-=======
         final RippleView rippleView = (RippleView) rootView.findViewById(R.id.addRipple);
         Button fabB = (Button) rootView.findViewById(R.id.fabbutton);
-        fabS = (ImageButton) rootView.findViewById(R.id.fabbuttonSearch);
->>>>>>> 483dd003a65394db344d80c10a3f2794c8441e13
+        fabS = (FloatingActionButton) rootView.findViewById(R.id.fabbuttonSearch);
 
         et1 = (EditText) rootView.findViewById(R.id.etFirst);
         et2 = (EditText) rootView.findViewById(R.id.etSecond);
         et3 = (EditText) rootView.findViewById(R.id.etThird);
 
-<<<<<<< HEAD
-=======
         Outline mOutlineCircle;
         int shapeSize = getResources().getDimensionPixelSize(R.dimen.button_elevation);
         mOutlineCircle = new Outline();
@@ -95,9 +89,8 @@ public class Have extends android.support.v4.app.Fragment {
             }
         });
 
->>>>>>> 483dd003a65394db344d80c10a3f2794c8441e13
 
-        fabBtn.setOnClickListener(new View.OnClickListener() {
+        fabS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 helpper++;
@@ -105,19 +98,17 @@ public class Have extends android.support.v4.app.Fragment {
             }
         });
 
-        fabSbtn.setOnClickListener(new View.OnClickListener() {
+        fabS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String q1=et1.getText().toString();
                 String q2=et2.getText().toString();
                 String q3=et3.getText().toString();
                 if(q1!="" || q2!="" || q3!="") {
-
                     ArrayList<Recept> aMovies = new ArrayList<Recept>();
                     adapterMovies = new ReceptiAdapter(getActivity(), aMovies);
-                    lvMovies.setAdapter(adapterMovies);
+                    lvRecepti.setAdapter(adapterMovies);
                     fetchBoxOfficeMovies(q1 + " " + q2 + " " + q3);
-
                 }
             }
         });
@@ -141,7 +132,7 @@ public class Have extends android.support.v4.app.Fragment {
             default:
                 //do-nothing
         }
-        fabSbtn.setVisibility(View.VISIBLE);
+        fabS.setVisibility(View.VISIBLE);
     }
 
 
@@ -159,7 +150,6 @@ public class Have extends android.support.v4.app.Fragment {
                     items = body.getJSONArray("recipes");
                     // Parse json array into array of model objects
                     ArrayList<Recept> movies = Recept.fromJson(items);
-                    Log.d("sve", String.valueOf(movies));
                     // Load model objects into the adapter which displays them
                     adapterMovies.addAll(movies);
                 } catch (JSONException e) {
@@ -171,8 +161,7 @@ public class Have extends android.support.v4.app.Fragment {
     }
 
     public void setupMovieSelectedListener() {
-
-        lvMovies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lvRecepti.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View item, int position, long rowId) {
 
